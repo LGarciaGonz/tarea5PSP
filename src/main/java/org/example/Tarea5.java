ef431ffee6bea3e789d15ca62d590e097a79f09b
LGarciaGonz <190077@iessanalberto.com>
2024-03-04 22:54:26 +0100

    commit final

--- src/main/java/org/example/Tarea5.java
+++ src/main/java/org/example/Tarea5.java
@@ -26,32 +26,35 @@ public class Tarea5 {
     }
 }
 
+// Clase que controla el los puntos de enfoque.
 class Control extends Thread {
-
     private PuntoEnfoque a_PuntoEnfoque;
 
+    // Constructor.
     public Control(PuntoEnfoque a_PuntoEnfoque) {
         this.a_PuntoEnfoque = a_PuntoEnfoque;
     }
 
     @Override
     public void run() {
-
+        // El hilo se ejecuta mientras la distancia entre los extremos sea mayor que la distancia mínima.
         while ((a_PuntoEnfoque.a_ExtremoIzquierdo - a_PuntoEnfoque.a_ExtremoDerecho) > a_PuntoEnfoque.a_DistanciaMinima) {
 
             try {
+                // Adquirir 2 permisos del semáforo.
                 a_PuntoEnfoque.l_Semaforo.acquire(2);
             } catch (InterruptedException e) {
-                throw new RuntimeException(e);
+                throw new RuntimeException(e);      // Informar del error producido.
             }
 
+            // Imprime información sobre los extremos y la distancia mínima.
             System.out.println("\n>>> Extremo derecho: " + a_PuntoEnfoque.a_ExtremoDerecho);
             System.out.println(">>> Extremo izquierdo: " + a_PuntoEnfoque.a_ExtremoIzquierdo);
             System.out.println(">>> Distancia mínima: " + a_PuntoEnfoque.a_DistanciaMinima);
             System.out.println("\n------------------------------------------");
 
             try {
-                Thread.sleep(500);
+                Thread.sleep(500); // Espera de 500 milisegundos entre las ejecuciones.
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
@@ -60,11 +63,13 @@ class Control extends Thread {
     }
 }
 
+// Clase que controla los extremos.
 class Extremo extends Thread {
 
     private PuntoEnfoque a_PuntoEnfoque;
     private String a_Nombre;
 
+    // Constructor.
     public Extremo(PuntoEnfoque a_PuntoEnfoque, String a_Nombre) {
         this.a_PuntoEnfoque = a_PuntoEnfoque;
         this.a_Nombre = a_Nombre;
@@ -72,21 +77,22 @@ class Extremo extends Thread {
 
     @Override
     public void run() {
-
+        // El hilo se ejecuta mientras la distancia entre los extremos sea mayor que la distancia mínima.
         while ((a_PuntoEnfoque.a_ExtremoIzquierdo - a_PuntoEnfoque.a_ExtremoDerecho) > a_PuntoEnfoque.a_DistanciaMinima) {
 
+            // Si es el extremo derecho, se mueve reduciendo su posición en un quinto.
             if (a_Nombre.equals("Derecho")) {
-
-                a_PuntoEnfoque.a_ExtremoDerecho = a_PuntoEnfoque.a_ExtremoDerecho - (a_PuntoEnfoque.a_ExtremoDerecho  / 5);
-
-            } else if (a_Nombre.equals("Izquierdo")) {
-                a_PuntoEnfoque.a_ExtremoIzquierdo = a_PuntoEnfoque.a_ExtremoIzquierdo - (a_PuntoEnfoque.a_ExtremoIzquierdo  / 5);
+                a_PuntoEnfoque.a_ExtremoDerecho -= (a_PuntoEnfoque.a_ExtremoDerecho / 5);
+            }
+            // Si es el extremo izquierdo, se mueve reduciendo su posición en un quinto.
+            else if (a_Nombre.equals("Izquierdo")) {
+                a_PuntoEnfoque.a_ExtremoIzquierdo -= (a_PuntoEnfoque.a_ExtremoIzquierdo / 5);
             }
 
-            a_PuntoEnfoque.l_Semaforo.release();
+            a_PuntoEnfoque.l_Semaforo.release(); // Libera el semáforo para que otro hilo pueda adquirirlo.
 
             try {
-                Thread.sleep(1000);
+                Thread.sleep(1000); // Espera de 1 segundo.
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
@@ -97,9 +103,9 @@ class Extremo extends Thread {
 // Clase que actúa como buzón.
 class PuntoEnfoque {
 
-    public double a_ExtremoDerecho = -5;
-    public double a_ExtremoIzquierdo = 5;
-    public double a_DistanciaMinima = 0.1;
-    public Semaphore l_Semaforo = new Semaphore(1);
+    public double a_ExtremoDerecho = -5; // Posición del extremo derecho.
+    public double a_ExtremoIzquierdo = 5; // Posición del extremo izquierdo.
+    public double a_DistanciaMinima = 0.1; // Distancia mínima permitida entre los extremos.
+    public Semaphore l_Semaforo = new Semaphore(1); // Semáforo para controlar los movimientos.
 
 }
